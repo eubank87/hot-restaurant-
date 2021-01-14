cd457c082230d93a64abef82158971fef4d22c8b
Daniel Sasser <desasser79@gmail.com>
2021-01-14 13:06:03 -0800

    updating from frontend Ashley

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,19 +22,19 @@ const waitlist = [{
 }]
 
 app.get("/", (req, res)=>{
-  res.send("Welcome to my site!")
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.get("/index", (req,res)=>{
-
+app.get("/home", (req,res)=>{
+  res.sendFile(path.join(__dirname, 'index.html'))
 })
 
 app.get("/tables", (req,res)=>{
-
+  res.sendFile(path.join(__dirname, 'tables.html'))
 })
 
-app.get("/reservations", (req,res)=>{
-
+app.get("/reservation", (req,res)=>{
+  res.sendFile(path.join(__dirname, 'reservation.html'))
 })
 
 app.get("/api/tables", (req,res)=>{
@@ -44,14 +45,24 @@ app.get("/api/reservations", (req,res)=>{
   res.json(reservations);
 })
 
+app.get("/api/waitlist", (req,res)=>{
+  res.json(waitlist);
+})
+
 app.post("/api/reservations", (req,res) => {
   // takes the data from the POST and saves it into a new variable
   const newReservation = req.body;
 
+  if (reservations.length < 5) {
+    reservations.push(newReservation);
+    newReservation.type = 'table';
+  } else {
+    waitlist.push(newReservation);
+    newReservation.type = 'waitlist';
+  }
   console.log(newReservation);
 
-  reservations.push(newReservation);
-
+  // send back JSON object of new reservation
   res.json(newReservation);
 })
 
