508c07973231522cac6ea4d2d2babcdc84e1c9e1
Daniel Sasser <desasser79@gmail.com>
2021-01-14 12:02:51 -0800

    working on stuffes

--- index.js
+++ index.js
@@ -1,18 +1,58 @@
-const http = require("http");
 const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const tables = [];
-const reservations = [];
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const tables = [{
+  testKey: 'testNameTables',
+  alsoTestKey: 'testNameTooTables'
+}];
+const reservations = [{
+  resName: 'dummyName',
+  tableNum: 'dummyTable'
+}];
+const waitlist = [{
+  testKey: 'testNameWait',
+  alsoTestKey: 'testNameTooWait'
+}]
 
 app.get("/", (req, res)=>{
-    res.send("Welcome to my site!")
+  res.send("Welcome to my site!")
 })
 
-app.get("/turtles", (req,res)=>{
+app.get("/index", (req,res)=>{
 
+})
+
+app.get("/tables", (req,res)=>{
+
+})
+
+app.get("/reservations", (req,res)=>{
+
+})
+
+app.get("/api/tables", (req,res)=>{
+  res.json(tables);
+})
+
+app.get("/api/reservations", (req,res)=>{
+  res.json(reservations);
+})
+
+app.post("/api/reservations", (req,res) => {
+  // takes the data from the POST and saves it into a new variable
+  const newReservation = req.body;
+
+  console.log(newReservation);
+
+  reservations.push(newReservation);
+
+  res.json(newReservation);
 })
 
 app.listen(PORT, function(){
